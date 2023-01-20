992892a3b50042d5e593e67edb043805f7aa4a67
Богдан К <epam.2@mail.ru>
2023-01-20 16:28:43 +0400

    Add 2 and 3 examples. Add russian comments.

--- PythonApplication1/PythonApplication1/Functional.py
+++ PythonApplication1/PythonApplication1/Functional.py
@@ -1,8 +1,8 @@
 # ������
 
-# 1. ������
-# ������������ ������ ����� K - ���������� �������. ����� �� ������ K ������� � �������: �������� ������ � ��� ����������. 
-# �������� ��� ������ � �������, ��� �������� ������ - ��� ����, � ���������� - ��������.
+# 1. Фрукты
+# Пользователь вводит число K - количество фруктов. Затем он вводит K фруктов в формате: название фрукта и его количество. 
+# Добавьте все фрукты в словарь, где название фрукта - это ключ, а количество - значение.
 
 def fruit ():
 	k = int(input('number of fruit: '))
@@ -17,9 +17,9 @@ def fruit ():
 	return print(dictionary)
 
 
-# 2. ������� � �������
-# ������������ ������ ����� N. ����� �� ������ ������ ������ (��� � �������) ����� N ������. �������� ������ friends � �������� � ���� N �������� � ������� name � age. 
-# ������� ������ �������� �� ������ � �������� ��� ���.
+# 2. Старший и младший
+# Пользователь вводит число N. Затем он вводит личные данные (имя и возраст) своих N друзей. Создайте список friends и добавьте в него N словарей с ключами name и age. 
+# Найдите самого младшего из друзей и выведите его имя.
 
 def create_fiends():
     def input_name():
@@ -50,7 +50,49 @@ def youngest_friend(list_friend):
     youngest_friend = min(dictionary_friend.values())
     
     print("youngest friend is", get_key(dictionary_friend, youngest_friend))
-		
 
 
-	 
+
+# 3. Еще немного о друзьях
+# Пользователь вводит число N. Затем он вводит личные данные (имя и возраст) своих N друзей. 
+# Создайте список friends и добавьте в него N словарей с ключами name и age. 
+# Выведите средний возраст всех друзей и самое длинное имя.
+
+def avg_age(list_friend):
+    
+    N = len(list_friend)
+    dictionary_friends = dict(list_friend)
+    avarage_age = round(sum(dictionary_friends.values()) / N, 0)
+
+    
+    print("avarage age  is", avarage_age)
+
+# find longest NAME in list
+def longest_name(list_friend):
+    
+    dictionary_friends = dict(list_friend)
+
+    ## get names your friends
+    list_names = dictionary_friends.keys()
+    
+    longest_name = longest_string_func(list_names) 
+        
+    
+    print("longest_name is", longest_name)
+ 
+# find longest STRING in list
+def longest_string_func(x):
+    max = 0
+    for i in x:
+        if len(i) > max: 
+            max = len(i)
+            longest_string = i
+        
+    return longest_string
+
+
+# 4. Английский словарь
+# "Пора учить английский язык", - сказал себе Степа и решил написать программу для изучения английского языка. 
+# Программа получает на вход слово на английском языке и несколько его переводов на русском языке. 
+# Составьте словарь, в котором ключ - это английское слово, а значение - это список русских слов. 
+
--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -4,7 +4,12 @@ import Functional
 #Functional.fruit()
 
 # 2. Старший и младший
-Functional.youngest_friend(Functional.create_fiends())
+#Functional.youngest_friend(Functional.create_fiends())
 
+# 3. Еще немного о друзьях
+friends = Functional.create_fiends()
 
+Functional.avg_age(friends)
+Functional.longest_name(friends)
 	
+# 4. Английский словарь
